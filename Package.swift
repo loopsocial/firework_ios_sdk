b327a5d176a541f892958003ea26fcd4ea53fb3f
Firework <loopops@users.noreply.github.com>
2026-05-08 05:14:25 +0000

    Update SDK to v1.43.0-beta.8

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FireworkVideo",
-            url: "https://github.com/loopsocial/firework_ios_sdk/releases/download/v1.43.0-beta.7/FireworkVideo-v1.43.0-beta.7.xcframework.zip",
-            checksum: "c6d962694f55f9ad5f4714c830c3e959e3e90abe3e66cd616be293b79f81ba89")
+            url: "https://github.com/loopsocial/firework_ios_sdk/releases/download/v1.43.0-beta.8/FireworkVideo-v1.43.0-beta.8.xcframework.zip",
+            checksum: "d791fe6671d2a7b50bcbe304221b5fa3f15ce939434fc9e26d13dbb0b118b43a")
     ]
 )
