9421c0e069ebe8405990786fb886ab41a7914934
Firework <loopops@users.noreply.github.com>
2026-05-25 08:46:09 +0000

    Update SDK to v1.43.3-beta.26

--- FireworkVideo.podspec
+++ FireworkVideo.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
   spec.name                     = "FireworkVideo"
-  spec.version                  = "1.43.3-beta.24"
+  spec.version                  = "1.43.3-beta.26"
   spec.summary                  = "FireworkVideoSDK"
   spec.homepage                 = "https://github.com/loopsocial/firework_ios_sdk"
   spec.license                  = { :text => "Copyright 2021 Loop Now Technologies, Inc.", :type => "Copyright" }
@@ -16,7 +16,7 @@ Pod::Spec.new do |spec|
   spec.default_subspec          = :none
 
   spec.script_phases = [
-    {
+    { 
       :name => 'Check FireworkVideoIVSSupport version',
       :script => '
       LIBRARY_NAME="FireworkVideoIVSSupport"
--- Package.swift
+++ Package.swift
@@ -9,32 +9,13 @@ let package = Package(
     ],
     products: [
         .library(name: "FireworkVideo",
-                 targets: ["FireworkVideo"]),
-        .library(name: "FireworkVideoStatic",
-                 targets: ["FireworkVideoStaticResources"])
+                 targets: ["FireworkVideo"])
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
             name: "FireworkVideo",
-            url: "https://github.com/loopsocial/firework_ios_sdk/releases/download/v1.43.3-beta.24/"
-                + "FireworkVideo-v1.43.3-beta.24.xcframework.zip",
-            checksum: "61de8edd06457067d4d01ab8bd62a4a98596edb4034d70f9bce742be72570f81"),
-        .binaryTarget(
-            name: "FireworkVideoStaticBinary",
-            url: "https://github.com/loopsocial/firework_ios_sdk/releases/download/v1.43.3-beta.24/"
-                + "FireworkVideo-static-v1.43.3-beta.24.xcframework.zip",
-            checksum: "ba88c984998abbb95442a66830d83f17c042debd013fa794bb54304ffd4b53ad"),
-        .target(
-            name: "FireworkVideoStaticResources",
-            dependencies: ["FireworkVideoStaticBinary"],
-            path: "Sources/FireworkVideoStaticResources",
-            resources: [
-                .copy("Resources/FireworkVideo.bundle"),
-                .copy("Resources/LiveStreamKit_ChatUI.bundle"),
-                .copy("Resources/LiveStreamKit_InteractionUI.bundle"),
-                .copy("Resources/LiveStreamKit_ProductUI.bundle")
-            ]
-        )
+            url: "https://github.com/loopsocial/firework_ios_sdk/releases/download/v1.43.3-beta.26/FireworkVideo-v1.43.3-beta.26.xcframework.zip",
+            checksum: "35bf1113a5d70085a49c32234f76db7382de98c62fde2b20719e14ec44d268e9")
     ]
 )
