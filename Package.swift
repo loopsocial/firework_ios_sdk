55901bd42c056d391fe17dd047e8db816b2472e4
Firework <loopops@users.noreply.github.com>
2026-05-26 07:53:45 +0000

    Update SDK to v1.43.3

--- Package.swift
+++ Package.swift
@@ -9,32 +9,13 @@ let package = Package(
     ],
     products: [
         .library(name: "FireworkVideo",
-                 targets: ["FireworkVideo"]),
-        .library(name: "FireworkVideoStatic",
-                 targets: ["FireworkVideoStaticResources"])
+                 targets: ["FireworkVideo"])
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
             name: "FireworkVideo",
-            url: "https://github.com/loopsocial/firework_ios_sdk/releases/download/v1.43.3-beta.30/"
-                + "FireworkVideo-v1.43.3-beta.30.xcframework.zip",
-            checksum: "9f87ed3711dbe0feb187b21fa5f8ead62a05e56736138d83b183a6202e2213ea"),
-        .binaryTarget(
-            name: "FireworkVideoStaticBinary",
-            url: "https://github.com/loopsocial/firework_ios_sdk/releases/download/v1.43.3-beta.30/"
-                + "FireworkVideo-static-v1.43.3-beta.30.xcframework.zip",
-            checksum: "a377edc8891b4246faeaaed0667aa74a3ddc177e9d46617efa16b1af5fa1c876"),
-        .target(
-            name: "FireworkVideoStaticResources",
-            dependencies: ["FireworkVideoStaticBinary"],
-            path: "Sources/FireworkVideoStaticResources",
-            resources: [
-                .copy("Resources/FireworkVideo.bundle"),
-                .copy("Resources/LiveStreamKit_ChatUI.bundle"),
-                .copy("Resources/LiveStreamKit_InteractionUI.bundle"),
-                .copy("Resources/LiveStreamKit_ProductUI.bundle")
-            ]
-        )
+            url: "https://github.com/loopsocial/firework_ios_sdk/releases/download/v1.43.3/FireworkVideo-v1.43.3.xcframework.zip",
+            checksum: "54d135b29688a507658e455560ae16ba63580f558a7f15992e0901660298e6ff")
     ]
 )
