7995a180ac5442f7fee97f491b8680d57f7d20d6
Firework <loopops@users.noreply.github.com>
2026-06-15 07:59:23 +0000

    Update SDK to v1.44.0-beta.8

--- Package.swift
+++ Package.swift
@@ -17,14 +17,14 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FireworkVideo",
-            url: "https://github.com/loopsocial/firework_ios_sdk/releases/download/v1.44.0-beta.7/"
-                + "FireworkVideo-v1.44.0-beta.7.xcframework.zip",
-            checksum: "02a55cc1478cac5c0a35468447a2d39338bd50b929c2cc480203df7e0ab4a40a"),
+            url: "https://github.com/loopsocial/firework_ios_sdk/releases/download/v1.44.0-beta.8/"
+                + "FireworkVideo-v1.44.0-beta.8.xcframework.zip",
+            checksum: "4c05cf79c53d219e7aaf6b166e46b03c97a5bb516c9316ea2dabe56180fe1168"),
         .binaryTarget(
             name: "FireworkVideoStaticBinary",
-            url: "https://github.com/loopsocial/firework_ios_sdk/releases/download/v1.44.0-beta.7/"
-                + "FireworkVideo-static-v1.44.0-beta.7.xcframework.zip",
-            checksum: "388fce4c60174931a7d9a5eee6e4f08bba36cced7a6f1b79c46174222c364020"),
+            url: "https://github.com/loopsocial/firework_ios_sdk/releases/download/v1.44.0-beta.8/"
+                + "FireworkVideo-static-v1.44.0-beta.8.xcframework.zip",
+            checksum: "d2d7e275342212dc08cb06f71b6985f43d29a137b2fa6c66df5ed8bf27e9a83f"),
         .target(
             name: "FireworkVideoStaticResources",
             dependencies: ["FireworkVideoStaticBinary"],
